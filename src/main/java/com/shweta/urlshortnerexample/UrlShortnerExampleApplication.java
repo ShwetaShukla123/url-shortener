18bf66b7f867c1fc8fd2fda9d7dc1f047bc16f67
Shweta <shweta_shukla1@persistent.com>
2022-01-31 00:13:07 +0530

    initial commit

---
+++ src/main/java/com/shweta/urlshortnerexample/UrlShortnerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.shweta.urlshortnerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortnerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortnerExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shweta/urlshortnerexample/UrlShortnerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shweta.urlshortnerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
